4da9c72d63d2c199e74a853110dff4822c0ac65d
selegebu@mussa <mussaselegebu@gmail.com>
2021-01-29 12:07:52 +0300

    login and home page completed

---
+++ login.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+
+<title> website </title>
+<h1> this is LOGIN</h1>
+<?php
+
+?>
+<p>my website is about Desktop</p>
+<nav>
+    <ul>
+        <li>
+            <a href=""> john</a>
+            <a href=""> mussa</a>
+            <a href=""> home </a>
+        </li>
+    </ul>
+</nav>
+
+</html>
